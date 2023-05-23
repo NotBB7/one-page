f5bfc264d43f9906551b7955a50409d026d810b1
NotBB <notice76610@gmail.com>
2023-05-23 11:55:04 +0200

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,26 @@
-function createGallery(numImages) {
-    var galleryContainer = document.createElement('div');
-    galleryContainer.className = 'gallery-container';
+  function createGallery(numImages) {
+    var galleryContainer = document.querySelector('.gallery-container');
   
     for (var i = 0; i < numImages; i++) {
       var imageContainer = document.createElement('div');
       imageContainer.className = 'image-container';
   
+      var randomId = Math.floor(Math.random() * 200); // Générer un ID aléatoire pour chaque image
+  
+      var imageLink = document.createElement('a');
+      imageLink.setAttribute('href', 'https://picsum.photos/id/' + randomId + '/800/600'); // Utilisez la source d'images de Picsum ici
+      imageLink.setAttribute('data-lightbox', 'gallery');
+  
       var image = document.createElement('img');
-      image.src = 'https://picsum.photos/200'; // Utilisez la source d'images de Picsum ici
+      image.src = 'https://picsum.photos/id/' + randomId + '/200';
       image.alt = 'Image ' + (i + 1);
   
-      imageContainer.appendChild(image);
+      imageLink.appendChild(image);
+      imageContainer.appendChild(imageLink);
       galleryContainer.appendChild(imageContainer);
-  
-      if ((i + 1) % 10 === 0) {
-        imageContainer.style.marginRight = '0'; // Supprime la marge à droite pour le dernier élément de chaque ligne
-      }
     }
   
-    document.body.appendChild(galleryContainer);
+    lightbox.init(); // Initialisation de Lightbox2
   }
   
-  createGallery(4); // Appel de la fonction avec le nombre d'images souhaité
-  
+  createGallery(12); // Appel de la fonction avec le nombre d'images souhaité
